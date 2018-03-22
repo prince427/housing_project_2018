3aac3969e7a4d5dbbdffffa51bab7da1c1a454ce
prince427 <30903160+prince427@users.noreply.github.com>
2018-03-22 10:55:16 +0000

    Test

--- corpusAnalysis.py
+++ corpusAnalysis.py
@@ -1,16 +1,20 @@
 import nltk
 from nltk.corpus import PlaintextCorpusReader
+from nltk.corpus import stopwords
+
 
 
 
 files = ".*\.txt"
-corpus0 = PlaintextCorpusReader(r"C:\Users\XMG\Documents\Work\Level 6 - 2017\Individual Project\Regeneration docs\Pre plan text", files)
+corpus0 = PlaintextCorpusReader(r"C:\Users\XMG\Documents\Work\Level 6 - 2017\Individual Project\Regeneration docs\Pre plan text",
+                                files)
 corpus = nltk.Text(corpus0.words())
 
+fdistcorpus = nltk.FreqDist(corpus)
 
 
-print (len(corpus))
-
+for sample in fdistcorpus:
+    print(sample)
 
 
 
