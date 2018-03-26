8ef248b376803c6b35c17675bf9837bf86ddaab7
prince427 <30903160+prince427@users.noreply.github.com>
2018-03-26 17:23:47 +0100

    Completed corpus analysis, copyright changes

--- corpusAnalysis.py
+++ corpusAnalysis.py
@@ -4,17 +4,20 @@ from nltk.corpus import stopwords
 
 
 
+stop_words = set(stopwords.words('english'))
 
 files = ".*\.txt"
 corpus0 = PlaintextCorpusReader(r"C:\Users\XMG\Documents\Work\Level 6 - 2017\Individual Project\Regeneration docs\Pre plan text",
                                 files)
 corpus = nltk.Text(corpus0.words())
+filtered_corpus = [w.lower() for w in corpus if not w in stop_words]
+filtered_corpus2 = [word.lower() for word in filtered_corpus if word.isalpha()]
+fdistcorpus = nltk.FreqDist(filtered_corpus2)
 
-fdistcorpus = nltk.FreqDist(corpus)
 
 
-for sample in fdistcorpus:
-    print(sample)
 
+
+print(fdistcorpus.most_common)
 
 
