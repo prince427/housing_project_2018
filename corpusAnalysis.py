28d9c0d6f1a5316c7e57d573aba59a9aed8a9f2b
prince427 <30903160+prince427@users.noreply.github.com>
2018-04-06 13:14:21 +0100

    Completed corpus analysis, copyright changes

--- corpusAnalysis.py
+++ corpusAnalysis.py
@@ -6,18 +6,48 @@ from nltk.corpus import stopwords
 
 stop_words = set(stopwords.words('english'))
 
-files = ".*\.txt"
-corpus0 = PlaintextCorpusReader(r"C:\Users\XMG\Documents\Work\Level 6 - 2017\Individual Project\Regeneration docs\Pre plan text",
-                                files)
-corpus = nltk.Text(corpus0.words())
-filtered_corpus = [w.lower() for w in corpus if not w in stop_words]
-filtered_corpus2 = [word.lower() for word in filtered_corpus if word.isalpha()]
-fdistcorpus = nltk.FreqDist(filtered_corpus2)
+
+# Pre plan
+files_pre = ".*\.txt"
+corpus_pre = PlaintextCorpusReader(r"C:\Users\XMG\Documents\Work\Level 6 - 2017\Individual Project\Regeneration docs\Pre plan text",
+                                files_pre)
+corpus2_pre = nltk.Text(corpus_pre.words())
+filtered_corpus_pre = [w.lower() for w in corpus2_pre if not w in stop_words]
+filtered_corpus2_pre = [word.lower() for word in filtered_corpus_pre if word.isalpha()]
+fdistcorpus_pre = nltk.FreqDist(filtered_corpus2_pre)
 
 
+# Design and planning
+files_des = ".*\.txt"
+corpus_des = PlaintextCorpusReader(r"C:\Users\XMG\Documents\Work\Level 6 - 2017\Individual Project\Regeneration docs\Design and planning text",
+                                files_des)
+corpus2_des = nltk.Text(corpus_des.words())
+filtered_corpus_des = [w.lower() for w in corpus2_des if not w in stop_words]
+filtered_corpus2_des = [word.lower() for word in filtered_corpus_des if word.isalpha()]
+fdistcorpus_des = nltk.FreqDist(filtered_corpus2_des)
 
 
+# Construction
+files_con = ".*\.txt"
+corpus_con = PlaintextCorpusReader(r"C:\Users\XMG\Documents\Work\Level 6 - 2017\Individual Project\Regeneration docs\Construction text",
+                                files_con)
+corpus2_con = nltk.Text(corpus_con.words())
+filtered_corpus_con = [w.lower() for w in corpus2_con if not w in stop_words]
+filtered_corpus2_con = [word.lower() for word in filtered_corpus_con if word.isalpha()]
+fdistcorpus_con = nltk.FreqDist(filtered_corpus2_con)
 
-print(fdistcorpus.most_common)
+
+# Post regeneration
+files_post = ".*\.txt"
+corpus_post = PlaintextCorpusReader(r"C:\Users\XMG\Documents\Work\Level 6 - 2017\Individual Project\Regeneration docs\Post regeneration text",
+                                files_post)
+corpus2_post = nltk.Text(corpus_post.words())
+filtered_corpus_post = [w.lower() for w in corpus2_post if not w in stop_words]
+filtered_corpus2_post = [word.lower() for word in filtered_corpus_post if word.isalpha()]
+fdistcorpus_post = nltk.FreqDist(filtered_corpus2_post)
 
 
+print(fdistcorpus_pre.most_common)
+print(fdistcorpus_des.most_common)
+print(fdistcorpus_con.most_common)
+print(fdistcorpus_post.most_common)
